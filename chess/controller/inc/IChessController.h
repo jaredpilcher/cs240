73df53dba6b1454d964536039c371366a7ed7b33
Jared Pilcher <jaredpilcher@yahoo.com>
2012-03-23 14:29:32 -0600

    Played around with IChessController.cpp. Won't compile. Seek TA's help

--- chess/controller/inc/IChessController.h
+++ chess/controller/inc/IChessController.h
@@ -3,8 +3,11 @@
 
 #include "IChessView.h"
 
+class ChessViewer;
+
 class IChessController
 {
+	ChessViewer * view;
 public:
 	
 	/**
@@ -16,57 +19,56 @@ public:
 	 * Indicate to the player that the user clicked on the given
 	 * row and column with the mouse.
 	 */
-	virtual void on_CellSelected(int row, int col, int button) = 0;
+	virtual void on_CellSelected(int row, int col, int button);
 	
 	///@param row where drag began
 	///@param col where drag began
-	virtual void on_DragStart(int row,int col) = 0;
+	virtual void on_DragStart(int row,int col);
 
 	///@param row where drag ended
 	///@param col where drag ended
 	///@return true if the drag resulted in a successful drop
-	virtual bool on_DragEnd(int row,int col) = 0;
+	virtual bool on_DragEnd(int row,int col);
 
 	/**
 	 * Handle when the user selected the new game button.
 	 */
-	virtual void on_NewGame() = 0;
+	virtual void on_NewGame();
 
 	/**
 	 * Handle when the user selected the save game button.
 	 */
-	virtual void on_SaveGame() = 0;
+	virtual void on_SaveGame();
 
 	/**
 	 * Handle when the user selected the save game as button.
 	 */
-	virtual void on_SaveGameAs() = 0;
+	virtual void on_SaveGameAs();
 
 	/**
 	 * Handle when the user selected the load game button.
 	 */
-	virtual void on_LoadGame() = 0;
-
+	virtual void on_LoadGame();
 	/**
 	 * Handle when the user selected the undo move button.
 	 */
-	virtual void on_UndoMove() = 0;
+	virtual void on_UndoMove();
 		
 	/**
 	 * Handle when the user selects to quit the game, either through the
 	 * quit button, the close X button, or the file menu.
 	 */
-	virtual void on_QuitGame() = 0;
+	virtual void on_QuitGame();
 		
 	/**
 	 * Handle when a timer event has been signaled.
 	 */
-	virtual void on_TimerEvent() = 0;
+	virtual void on_TimerEvent();
 	
 	/**
 	 * Set the IChessView that this IChessController will handle inputs for.
 	 */
-	virtual void SetView(IChessView* view) = 0;
+	virtual void SetView(IChessView* view);
 };
 
 #endif
---
+++ chess/controller/src/IChessController.cpp
@@ -0,0 +1,79 @@
+#include "IChessController.h"
+
+/**
+ * Indicate to the player that the user clicked on the given
+ * row and column with the mouse.
+ */
+virtual void on_CellSelected(int row, int col, int button){
+	
+}
+
+///@param row where drag began
+///@param col where drag began
+virtual void on_DragStart(int row,int col){
+
+}
+
+///@param row where drag ended
+///@param col where drag ended
+///@return true if the drag resulted in a successful drop
+virtual bool on_DragEnd(int row,int col){
+	return false;
+}
+
+/**
+ * Handle when the user selected the new game button.
+ */
+virtual void on_NewGame(){
+
+}
+
+/**
+ * Handle when the user selected the save game button.
+ */
+virtual void on_SaveGame(){
+
+}
+
+/**
+ * Handle when the user selected the save game as button.
+ */
+virtual void on_SaveGameAs(){
+
+}
+
+/**
+ * Handle when the user selected the load game button.
+ */
+virtual void on_LoadGame(){
+	view->SelectLoadFile();
+}
+
+/**
+ * Handle when the user selected the undo move button.
+ */
+virtual void on_UndoMove(){
+
+}
+	
+/**
+ * Handle when the user selects to quit the game, either through the
+ * quit button, the close X button, or the file menu.
+ */
+virtual void on_QuitGame(){
+
+}
+	
+/**
+ * Handle when a timer event has been signaled.
+ */
+virtual void on_TimerEvent(){
+
+}
+
+/**
+ * Set the IChessView that this IChessController will handle inputs for.
+ */
+virtual void SetView(IChessView* view){
+
+}
